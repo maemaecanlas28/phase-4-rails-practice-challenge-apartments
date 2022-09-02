6efc16267f6996981932b18259d1377039ea9f00
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-09-01 23:36:36 -0700

    DONE with the assignment

---
+++ app/controllers/leases_controller.rb
@@ -0,0 +1,22 @@
+class LeasesController < ApplicationController
+    
+    # rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    def create
+        lease = Lease.create!(params.permit(:apartment_id, :tenant_id, :rent))
+        render json: lease, status: :created
+    end
+
+    def destroy
+        lease = Lease.find(params[:id])
+        lease.destroy
+        head :no_content
+    end
+
+    private
+
+    # def render_unprocessable_entity_response(exception)
+    #     render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+    # end
+
+end
